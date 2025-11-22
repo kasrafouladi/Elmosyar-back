b12ac5a36890a5a4e5eaa1ee60553278cb269470
Kasra Fouladi <152831297+kasrafouladi@users.noreply.github.com>
2025-11-22 23:34:41 +0330

    debugging

--- elmosyar_back/core/urls.py
+++ elmosyar_back/core/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Index
     path('', views.index, name='index'),
     
     # Authentication
     path('signup/', views.signup, name='signup'),
-    path('login/', views.Login.as_view(), name='login'),
-    path('logout/', views.Logout.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 
     path('token/verify/', views.VerifyTokenView.as_view(), name='token_verify'),
     path('token/refresh/', views.RefreshTokenView.as_view(), name='token_refresh'),
